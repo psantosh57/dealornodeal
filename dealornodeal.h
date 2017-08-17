5eccfe91f5ea1c90ddfc854c95286ed8b6218bba
Santosh Prabhu <psantosh57@yahoo.com>
2017-08-16 23:13:11 -0700

    Interactive fully working

--- dealornodeal.h
+++ dealornodeal.h
@@ -6,22 +6,64 @@ public:
 
 	suitcase(int m = 0) :_money(m) {
 
-		cout << "In suitcase constructor " << endl;
+		//cout << "In suitcase constructor " << endl;
 	}
 
 	~suitcase() {
 
-		cout << "In suitcase destructor" << endl;
+		//cout << "In suitcase destructor" << endl;
 	}
 
-	suitcase& operator=(suitcase& rhs) {
+	suitcase& operator=(int rhs) {
 
-		cout << "In = operator" << endl;
+		//cout << "In = operator" << endl;
 
-		this->_money = rhs._money;
+		this->_money = rhs;
 
 		return *this;
 
+	}
+
+	friend ostream& operator<<(ostream& o, suitcase s) {
+
+		o << s._money << endl;
+
+		return o;
+	}
+
+	bool operator==(int rhs) {
+
+		return true ? this->_money == rhs : false;
+	}
+
+	bool operator!=(suitcase& rhs) {
+
+		return !(*this == rhs);
+	}
+
+	operator int() const {
+
+		//cout << "In int()" << endl;
+
+		return this->_money;
+	}
+
+	int operator+(int rhs) {
+
+		suitcase sum;
+
+		sum._money = this->_money + rhs;
+
+		return sum._money;
+	}
+
+	int operator/(int rhs) {
+
+		suitcase div;
+
+		div._money = this->_money / rhs;
+
+		return div._money;
 	}
 
 	
@@ -31,30 +73,46 @@ private:
 	
 };
 
-class list_of_suitcases {
+class game {
 
 public:
 
-	list_of_suitcases(suitcase s[]) :_suits{ 0 } {
+	game(suitcase l[]) :_s{ 0 } {
 
-		cout << "In list_of_suitcases constructor" << endl;
+		//cout << "In game constructor" << endl;
+		for (int i = 0; i < 26; i++) {
+
+			_s[i] = l[i];
+		}
+	}
+
+	~game() {
+
+		//cout << "In game destructor" << endl;
+	}
+
+	friend ostream& operator<<(ostream& o, game g) {
 
 		for (int i = 0; i < 26; i++) {
 
-			_suits[i] = s[i];
-		}
+			o << g._s[i] << endl;
 
-		cout << "Filled array" << endl;
+		}		
 
+		return o;
 	}
 
-	~list_of_suitcases() {
+	void shuffleArray();
+	void revealCases(int num, int myCase, int roundNum);
+	void showRemainingCases(int myCase);
+	bool generateOffer(int myCase);
+	bool playRounds(int myCase, bool isInteractive = false);
+	void playGame(bool isInteractive = false);
 
-		cout << "In list_of_suitcases destructor" << endl;
-	}
+	static bool ifInteractive;
 
 private:
 
-	suitcase _suits[26];
-
+	//list_of_suitcases _list;
+	suitcase _s[26];
 };
